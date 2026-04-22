eafc5e719a1916b1ecc45b7c2740f1d32d354fb2
fakhreddinelargou <largofakhrddin@gmail.com>
2026-04-22 16:47:30 +0100

    add echo.js to vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js' , 'resources/js/sidbar.js',],
+            input: ['resources/css/app.css', 'resources/js/app.js' , 'resources/js/sidbar.js','resources/js/echo.js'],
             refresh: true,
         }),
         tailwindcss(),
